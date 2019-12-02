c44e93bc82e28a19c0232b0a2936f19a0182af26
root <root@classroom.example.com>
2019-12-02 16:05:02 +0530

    first commit

---
+++ openstack.sh
@@ -0,0 +1,17 @@
+yum update -y
+systemctl stop postfix firewalld NetworkManager
+systemctl disable postfix firewalld NetworkManager
+systemctl mask NetworkManager
+yum remove postfix NetworkManager NetworkManager-libnm -y
+yum install ntpdate -y
+yum install https://www.rdoproject.org/repos/rdo-release.rpm -y
+yum update –y
+yum install  openstack-packstack -y
+packstack --gen-answer-file=openstack.conf
+sed -i 's/CONFIG_NTP_SERVERS=/CONFIG_NTP_SERVERS=0.ro.pool.ntp.org/g' openstack.conf
+sed -i 's/CONFIG_PROVISION_DEMO=y/CONFIG_PROVISION_DEMO=n/g' openstack.conf
+sed -i 's/CONFIG_HORIZON_SSL=n/CONFIG_HORIZON_SSL=y/g' openstack.conf
+sed -i 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
+packstack --answer-file openstack.conf
+
